1dbf9a3f312fa9d51d526db2b78dded596aa3767
phanuwat-cht <phanuwat.cht@gmail.com>
2025-01-26 14:07:46 +0700

    manual upload streamlit file

--- app.py
+++ app.py
@@ -13,6 +13,6 @@ import random
 st.title('Test Streamlit')
 st.write('Hello World!')
 
-if st.button('Generate Random Number')
-  random_number = random.randint(1, 100)
-  st.write(f'Random Number: {random_number}')
+if st.button('Generate Random Number'):
+    random_number = random.randint(1, 100)
+    st.write(f'Random Number: {random_number}')
